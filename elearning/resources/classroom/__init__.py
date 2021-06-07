9f613e9f813b4fcdc3107f36a5362509a8c040fc
Rizal Ilham <rzlilhm09@gmail.com>
2021-06-08 05:00:57 +0700

    Initialize TheoryResource

--- elearning/resources/classroom/__init__.py
+++ elearning/resources/classroom/__init__.py
@@ -1,6 +1,7 @@
-from .views import ClassroomsResource, ClassroomResource
+from .views import ClassroomsResource, ClassroomResource, TheoryResource
 
 __all__ = [
     "ClassroomsResource",
     "ClassroomResource",
+    'TheoryResource',
 ]
