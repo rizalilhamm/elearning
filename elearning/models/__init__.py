6d9231bfb9a947c74d4a3fdcf0ed74afcebc1a9e
Rizal Ilham <rzlilhm09@gmail.com>
2021-06-08 02:14:20 +0700

    Update __init__.py file

--- elearning/models/__init__.py
+++ elearning/models/__init__.py
@@ -1,8 +1,9 @@
-from .databasemodels import User, Class, user_identifier, Tasks
+from .databasemodels import User, Class, user_identifier, Tasks, Theory
 
 __all__ = [
     'User',
     'Class',
     'student_identifier',
     'Tasks',
+    'Theory'
 ]
