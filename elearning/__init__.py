b61512f173133e39f9ae8b78adbb76b3d93e7054
Rizal Ilham <rzlilhm09@gmail.com>
2021-06-27 13:11:48 +0700

    Manual Register for input user data

--- elearning/__init__.py
+++ elearning/__init__.py
@@ -1,10 +1,11 @@
 import os
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template, flash, redirect
+from flask.helpers import url_for
 from flask.wrappers import JSONMixin
-from flask_restful import Api, Resource
+from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 
 from elearning.resources import errors
 from elearning.config import Config
@@ -20,11 +21,51 @@ login.login_view = '/account/login'
 login.login_message = 'You have to login to access class'
 db = SQLAlchemy(elearning)
 
-@elearning.route('/account/getaja', methods=['POST', 'GET'])
-def get_aja():
-    return jsonify({
-        'data': 'Kasmadi Ramadhan'
-    })
+
+user_email_domain = ['lecture.ar-raniry.ac.id', 'student.ar-raniry.ac.id']
+@elearning.route('/manual-register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+        user_level = None
+        
+        if (firstname.replace(' ', '') or lastname.replace(' ', '') or email or password) == '':
+            flash('Semua field wajib diisi!')
+            return redirect(url_for('register'))
+        
+        _, email_domain = email.split('@')
+        for i in range(len(user_email_domain)):
+            if email_domain == user_email_domain[i]:
+                if i == 0:
+                    user_level = 1
+                elif i == 1:
+                    user_level = 2
+                break
+        
+        if User.query.filter_by(email=email).first():
+            flash('Email sudah terdaftar coba yang lain')
+            return redirect(url_for('register'))
+
+        if user_level == None:
+            flash('Email tidak diizinkan')
+            return redirect(url_for('register'))
+            
+        if (password != confirm_password) or (len(password) != len(confirm_password)):
+            flash('Password harus sama')
+            return redirect(url_for('register'))
+        
+        new_user = User(firstname=firstname, lastname=lastname, email=email, user_level=user_level)
+        new_user.hash_password()
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Register Berhasil, register another email!')
+        return redirect(url_for('register'))
+
+    return render_template('register.html')
 
 db.create_all()
 
