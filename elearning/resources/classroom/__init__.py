ee84fd3388e31702258cf3524ad2d5b6bf46e050
Rizal Ilham <rzlilhm09@gmail.com>
2021-07-06 09:05:37 +0700

    update: Classroom/views.py update variable name

--- elearning/resources/classroom/__init__.py
+++ elearning/resources/classroom/__init__.py
@@ -1,7 +1,10 @@
 from .views import ClassroomsResource, ClassroomResource, MaterialsResource
+from .archive_class import ArchivedsClassroom, ArchivedClassroom
 
 __all__ = [
     "ClassroomsResource",
     "ClassroomResource",
     'MaterialsResource',
+    'ArchivedsClassroom',
+    'ArchivedClassroom',
 ]
